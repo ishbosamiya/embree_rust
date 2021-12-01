b550623dfc8a07055f73b73c119bc5a70f493a80
ishbosamiya <ishbosamiya@gmail.com>
2021-12-01 08:50:05 +0530

    build: allow only rtc types and functions

--- build.rs
+++ build.rs
@@ -118,6 +118,8 @@ fn main() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .allowlist_type("RTC.*")
+        .allowlist_function("rtc.*")
         .generate()
         .expect("Unable to generate bindings");
 
