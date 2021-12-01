0047ffac3570eb7969b5c9a5bd31d1b183a0e47b
ishbosamiya <ishbosamiya@gmail.com>
2021-12-01 20:15:11 +0530

    examples: simple_triangle: use the safe API

--- examples/simple_triangle.rs
+++ examples/simple_triangle.rs
@@ -1,107 +1,44 @@
-use std::os::raw::c_uint;
-
-use embree_rust::{sys, Vec3};
+use embree_rust::{
+    Device, Geometry, GeometryTriangle, Ray, Scene, SceneCommited, Triangle, Vec3, Vert,
+    INVALID_GEOMETRY_ID,
+};
 use image::Pixel;
 
-fn generate_cube_verts_and_indices() -> (Vec<(f32, f32, f32)>, Vec<c_uint>) {
+fn generate_cube() -> (Vec<Vert>, Vec<Triangle>) {
     let cube_verts = vec![
-        (1.0, 1.0, 1.0),
-        (1.0, 1.0, -1.0),
-        (1.0, -1.0, 1.0),
-        (1.0, -1.0, -1.0),
-        (-1.0, 1.0, 1.0),
-        (-1.0, 1.0, -1.0),
-        (-1.0, -1.0, 1.0),
-        (-1.0, -1.0, -1.0),
+        Vert::new(Vec3::new(1.0, 1.0, 1.0)),
+        Vert::new(Vec3::new(1.0, 1.0, -1.0)),
+        Vert::new(Vec3::new(1.0, -1.0, 1.0)),
+        Vert::new(Vec3::new(1.0, -1.0, -1.0)),
+        Vert::new(Vec3::new(-1.0, 1.0, 1.0)),
+        Vert::new(Vec3::new(-1.0, 1.0, -1.0)),
+        Vert::new(Vec3::new(-1.0, -1.0, 1.0)),
+        Vert::new(Vec3::new(-1.0, -1.0, -1.0)),
     ];
 
     let cube_indices = vec![
-        0, 3, 1, 0, 2, 3, 2, 7, 3, 2, 6, 3, 4, 2, 0, 4, 6, 2, 3, 5, 7, 3, 1, 5, 5, 7, 6, 5, 1, 4,
-        1, 5, 4, 1, 4, 0,
+        Triangle::new(0, 3, 1),
+        Triangle::new(0, 2, 3),
+        Triangle::new(2, 7, 3),
+        Triangle::new(2, 6, 3),
+        Triangle::new(4, 2, 0),
+        Triangle::new(4, 6, 2),
+        Triangle::new(3, 5, 7),
+        Triangle::new(3, 1, 5),
+        Triangle::new(5, 7, 6),
+        Triangle::new(5, 1, 4),
+        Triangle::new(1, 5, 4),
+        Triangle::new(1, 4, 0),
     ];
 
     (cube_verts, cube_indices)
 }
 
-fn embree_generate_cube(device: sys::RTCDevice) -> sys::RTCGeometry {
-    let (cube_verts, cube_indices) = generate_cube_verts_and_indices();
-
-    let geometry =
-        unsafe { sys::rtcNewGeometry(device, sys::RTCGeometryType_RTC_GEOMETRY_TYPE_TRIANGLE) };
-
-    let verts: &mut [(f32, f32, f32)] = unsafe {
-        std::slice::from_raw_parts_mut(
-            sys::rtcSetNewGeometryBuffer(
-                geometry,
-                sys::RTCBufferType_RTC_BUFFER_TYPE_VERTEX,
-                0,
-                sys::RTCFormat_RTC_FORMAT_FLOAT3,
-                std::mem::size_of::<(f32, f32, f32)>().try_into().unwrap(),
-                8,
-            ) as *mut (f32, f32, f32),
-            8,
-        )
-    };
-
-    verts
-        .iter_mut()
-        .zip(cube_verts.iter())
-        .for_each(|(v1, v2)| {
-            v1.0 = v2.0;
-            v1.1 = v2.1;
-            v1.2 = v2.2;
-        });
-
-    let indices: &mut [(c_uint, c_uint, c_uint)] = unsafe {
-        std::slice::from_raw_parts_mut(
-            sys::rtcSetNewGeometryBuffer(
-                geometry,
-                sys::RTCBufferType_RTC_BUFFER_TYPE_INDEX,
-                0,
-                sys::RTCFormat_RTC_FORMAT_UINT3,
-                std::mem::size_of::<(c_uint, c_uint, c_uint)>()
-                    .try_into()
-                    .unwrap(),
-                12,
-            ) as *mut (c_uint, c_uint, c_uint),
-            12,
-        )
-    };
-
-    indices
-        .iter_mut()
-        .zip(cube_indices.chunks(3))
-        .for_each(|(i, ci)| {
-            i.0 = ci[0];
-            i.1 = ci[1];
-            i.2 = ci[2];
-        });
-
-    unsafe {
-        sys::rtcSetGeometryBuildQuality(geometry, sys::RTCBuildQuality_RTC_BUILD_QUALITY_HIGH);
-    }
-
-    unsafe {
-        sys::rtcCommitGeometry(geometry);
-    }
-
-    geometry
-}
-
-fn trace_ray(ray: sys::RTCRay, scene: sys::RTCScene) -> sys::RTCRayHit {
-    let mut context = sys::RTCIntersectContext::default();
-
-    let mut rayhit = sys::RTCRayHit {
-        ray,
-        hit: sys::RTCHit::default(),
-    };
-
-    unsafe { sys::rtcIntersect1(scene, &mut context, &mut rayhit) }
-
-    rayhit
-}
-
-fn trace_image(scene: sys::RTCScene, width: usize, height: usize) -> image::DynamicImage {
+fn trace_image(
+    scene: &Scene<'_, SceneCommited>,
+    width: usize,
+    height: usize,
+) -> image::DynamicImage {
     let camera_origin = (0.0, 0.0, -3.0);
     let camera_focal_length = 6.0;
     let camera_horizontal = 5.0;
@@ -120,18 +57,15 @@ fn trace_image(scene: sys::RTCScene, width: usize, height: usize) -> image::Dyna
                 camera_focal_length,
             );
 
-            let ray_hit = trace_ray(
-                sys::RTCRay::new(
-                    Vec3::new(camera_origin.0, camera_origin.1, camera_origin.2),
-                    0.001,
-                    1000.0,
-                    Vec3::new(ray_direction.0, ray_direction.1, ray_direction.2),
-                    0.0,
-                ),
-                scene,
-            );
+            let ray_hit = scene.intersect(Ray::new(
+                Vec3::new(camera_origin.0, camera_origin.1, camera_origin.2),
+                0.001,
+                1000.0,
+                Vec3::new(ray_direction.0, ray_direction.1, ray_direction.2),
+                0.0,
+            ));
 
-            let rgb = if ray_hit.hit.geomID != sys::RTC_INVALID_GEOMETRY_ID {
+            let rgb = if ray_hit.hit.geomID != INVALID_GEOMETRY_ID {
                 [ray_hit.hit.u, ray_hit.hit.v, 0.0]
             } else {
                 [0.0, 0.0, 0.0]
@@ -148,28 +82,24 @@ fn trace_image(scene: sys::RTCScene, width: usize, height: usize) -> image::Dyna
 }
 
 fn main() {
-    let device = unsafe { sys::rtcNewDevice(std::ptr::null()) };
+    let device = Device::new();
 
-    let scene = unsafe { sys::rtcNewScene(device) };
+    let mut scene = Scene::new(&device);
 
-    let cube_geometry = embree_generate_cube(device);
+    let (cube_verts, cube_triangles) = generate_cube();
+    let cube_geometry =
+        Geometry::Triangle(GeometryTriangle::new(&device, &cube_verts, &cube_triangles));
 
-    let _cube_id = unsafe { sys::rtcAttachGeometry(scene, cube_geometry) };
+    scene.attach_geometry(&cube_geometry);
 
-    unsafe {
-        sys::rtcReleaseGeometry(cube_geometry);
-    }
-
-    unsafe {
-        sys::rtcCommitScene(scene);
-    }
+    let scene = scene.commit();
 
     let viuer_config = viuer::Config {
         absolute_offset: false,
         ..Default::default()
     };
 
-    let image = trace_image(scene, 100, 100);
+    let image = trace_image(&scene, 100, 100);
 
     viuer::print(&image, &viuer_config).unwrap();
 }
